e6c815035eb6701d7e93d1273128b8fbab897f26
TonyThorne <anthony.thorne@gmail.com>
2023-01-04 14:59:46 +0000

    Feat: Updated response text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   const name = process.env.NAME || "World";
-  res.send(`Hello to Tony's ${name}!`);
+  res.send(`Hello to Tony's Continuous Delivery ${name}!`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
