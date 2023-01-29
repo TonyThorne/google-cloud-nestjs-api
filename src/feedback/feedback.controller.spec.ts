3fa41bb67c6c4723e0135c1da1c593650216ec28
TonyThorne <anthony.thorne@gmail.com>
2023-01-29 17:28:59 +0000

    Test: Added basic test for feedback service

--- src/feedback/feedback.controller.spec.ts
+++ src/feedback/feedback.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FeedbackController } from './feedback.controller';
+import { FeedbackService } from './feedback.service';
 
 describe('FeedbackController', () => {
   let controller: FeedbackController;
@@ -7,6 +8,7 @@ describe('FeedbackController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FeedbackController],
+      providers: [FeedbackService],
     }).compile();
 
     controller = module.get<FeedbackController>(FeedbackController);
