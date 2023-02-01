4658253029f70a27d7d518ddc36a8e423e89c841
TonyThorne <anthony.thorne@gmail.com>
2023-02-01 20:58:28 +0000

    Feat: Linked to Monday with GraphQL

--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -2,14 +2,12 @@ import { Injectable, Logger } from '@nestjs/common';
 import { FeedbackData } from './feedback.model';
 import { HttpService } from '@nestjs/axios';
 import { catchError, Observable, map, tap } from 'rxjs';
-// import { AxiosResponse } from 'axios';
 @Injectable()
 export class FeedbackService {
   constructor(private readonly httpService: HttpService) { }
-  // private feedback: FeedbackData[] = [];
 
   getAllFeedback(): Observable<FeedbackData[]> {
-    Logger.log('info get all feedback')
+    Logger.log('info get all feedback');
     return this.httpService
       .get('https://test-api-lvpopocrba-nw.a.run.app/feedback')
       .pipe(map((response) => response.data));
@@ -38,19 +36,29 @@ export class FeedbackService {
         'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIxNjc0MDM0OSwidWlkIjozNzc4OTUxMSwiaWFkIjoiMjAyMy0wMS0wNFQxMToxMDoxNS4zNTJaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTQ2NTgyNTksInJnbiI6InVzZTEifQ.UfFLrhJT67Grh6q5WsQUNyesDtC1N4zY2Q2YiYJ7nUk',
     };
 
-    const mondayMutation = `
-    mutation($name: String) {
-    create_item (board_id: 3748755036, group_id: "topics", item_name: $name
-    ) {
-        id
-        name
-}
-    }`;
+    const today = new Date().toISOString().split("T")[0]
+
+    const vars = {
+      myItemName: feedbackData.name,
+      column_values: JSON.stringify({
+        date4: today,
+        status: { label: 'Add to backlog' },
+        email: { email: feedbackData.email, text: feedbackData.email },
+        text8: feedbackData.subject,
+        long_text: feedbackData.details
+      }),
+    }
+    const mondayMutation5 = `mutation ($myItemName: String!, $column_values: JSON!){ create_item(board_id: 3748755036, group_id: "topics", item_name: $myItemName, 
+        column_values: $column_values
+        ) {  id  }  }`
 
     return this.httpService
       .post(
         'https://api.monday.com/v2',
-        { query: mondayMutation, variables: { name: feedbackData.name } },
+        {
+          query: mondayMutation5,
+          variables: JSON.stringify(vars)
+        },
         {
           headers: headers,
         },
