5dec10cc892df9322d2b4950cc245add87d5c2d3
TonyThorne <anthony.thorne@gmail.com>
2023-04-06 16:18:24 +0100

    Test: Errors GPC side removed Monday integration

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -16,29 +16,29 @@ export class FeedbackController {
   constructor(private feedbackService: FeedbackService) { }
 
   @Get()
-  getAllFeedback(): Observable<FeedbackData[]> {
+  getAllFeedback(): FeedbackData {
     return this.feedbackService.getAllFeedback()
   }
 
-  @Post()
-  async createFeedback(
-    @Body('dateTime') dateTime: Date,
-    @Body('name') name: string,
-    @Body('email') email: string,
-    @Body('subject') subject: string,
-    @Body('details') details: string,
-  ): Promise<Observable<FeedbackData>> {
-    try {
-      const result = await this.feedbackService.createFeedback(
-        dateTime,
-        name,
-        email,
-        subject,
-        details,
-      )
-      return result
-    } catch (error) {
-      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
-    }
-  }
+  // @Post()
+  // async createFeedback(
+  //   @Body('dateTime') dateTime: Date,
+  //   @Body('name') name: string,
+  //   @Body('email') email: string,
+  //   @Body('subject') subject: string,
+  //   @Body('details') details: string,
+  // ): Promise<Observable<FeedbackData>> {
+  //   try {
+  //     const result = await this.feedbackService.createFeedback(
+  //       dateTime,
+  //       name,
+  //       email,
+  //       subject,
+  //       details,
+  //     )
+  //     return result
+  //   } catch (error) {
+  //     throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
+  //   }
+  // }
 }
--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common'
+import { Catch, Injectable, Logger } from '@nestjs/common'
 import { FeedbackData } from './feedback.model'
 import { HttpService } from '@nestjs/axios'
 import { catchError, Observable, map, tap } from 'rxjs'
-import { uuid } from 'uuidv4'
+import { v4 as uuid } from 'uuid'
 import { secretesFactory } from '../utils/secretesFactory'
 
 @Injectable()
@@ -10,93 +10,107 @@ export class FeedbackService {
   // eslint-disable-next-line prettier/prettier
   constructor(private readonly httpService: HttpService) { }
 
-  getAllFeedback(): Observable<FeedbackData[]> {
+  getAllFeedback(): FeedbackData {
     Logger.log('info get all feedback')
-    return this.httpService
-      .get('https://test-api-lvpopocrba-nw.a.run.app/feedback')
-      .pipe(map((response) => response.data))
-  }
-
-  async mondayapi(): Promise<object> {
     try {
-      const res = await secretesFactory('google')
-      const headers = {
-        'Content-Type': 'application/json',
-        Authorization: res,
-      }
-      return headers
-    } catch (error) {
-      Logger.error('Error in mondayapi:', error)
-      throw new Error(
-        'Failed to get Monday API headers from Google secretes store.',
-      )
-    }
-  }
-
-  async createFeedback(
-    dateTime: Date,
-    name: string,
-    email: string,
-    subject: string,
-    details: string,
-  ): Promise<any> {
-    try {
-      const feedbackData: FeedbackData = {
-        id: uuid(),
-        dateTime: dateTime,
-        name: name,
-        email: email,
-        subject: subject,
-        details: details,
+      const res = {
+        id: 'example id',
+        dateTime: new Date(),
+        name: 'string',
+        email: 'string',
+        subject: 'string',
+        details: 'string',
       }
 
-      const headers = await this.mondayapi()
-      return this.sendFeedbackToMonday(feedbackData, headers)
+      return res
     } catch (error) {
-      Logger.error('Error in createFeedback:', error)
-      throw new Error('Failed to create feedback.')
+      Logger.error('Error in getAllFeedback', error)
     }
+    // return this.httpService
+    //   .get('https://test-api-lvpopocrba-nw.a.run.app/feedback')
+    //   .pipe(map((response) => response.data))
   }
 
-  sendFeedbackToMonday(
-    feedbackData: FeedbackData,
-    headers: any,
-  ): Observable<any> {
-    const today = new Date().toISOString().split('T')[0]
+  // async mondayapi(): Promise<object> {
+  //   try {
+  //     const res = await secretesFactory('google')
+  //     const headers = {
+  //       'Content-Type': 'application/json',
+  //       Authorization: res,
+  //     }
+  //     return headers
+  //   } catch (error) {
+  //     Logger.error('Error in mondayapi:', error)
+  //     throw new Error(
+  //       'Failed to get Monday API headers from Google secretes store.',
+  //     )
+  //   }
+  // }
 
-    const vars = {
-      myItemName: feedbackData.name,
-      column_values: JSON.stringify({
-        date4: today,
-        status: { label: 'Add to backlog' },
-        email: { email: feedbackData.email, text: feedbackData.email },
-        text8: feedbackData.subject,
-        long_text: feedbackData.details,
-        text6: feedbackData.id,
-      }),
-    }
-    const mondayMutation5 = `mutation ($myItemName: String!, $column_values: JSON!){ create_item(board_id: 3748755036, group_id: "topics", item_name: $myItemName, 
-        column_values: $column_values
-        ) {  id  }  }`
+  // async createFeedback(
+  //   dateTime: Date,
+  //   name: string,
+  //   email: string,
+  //   subject: string,
+  //   details: string,
+  // ): Promise<any> {
+  //   try {
+  //     const feedbackData: FeedbackData = {
+  //       id: uuid(),
+  //       dateTime: dateTime,
+  //       name: name,
+  //       email: email,
+  //       subject: subject,
+  //       details: details,
+  //     }
 
-    return this.httpService
-      .post(
-        'https://api.monday.com/v2',
-        {
-          query: mondayMutation5,
-          variables: JSON.stringify(vars),
-        },
-        {
-          headers: headers,
-        },
-      )
-      .pipe(
-        catchError((error: any) => {
-          Logger.error('Error in sendFeedbackToMonday:', error.response.data)
-          throw 'An error happened!'
-        }),
-        tap((response) => Logger.log('info', response.data)),
-        map((response) => response.data),
-      )
-  }
+  //     const headers = await this.mondayapi()
+  //     return this.sendFeedbackToMonday(feedbackData, headers)
+  //   } catch (error) {
+  //     Logger.error('Error in createFeedback:', error)
+  //     throw new Error('Failed to create feedback.')
+  //   }
+  // }
+
+  // sendFeedbackToMonday(
+  //   feedbackData: FeedbackData,
+  //   headers: any,
+  // ): Observable<any> {
+  //   const today = new Date().toISOString().split('T')[0]
+
+  //   const vars = {
+  //     myItemName: feedbackData.name,
+  //     column_values: JSON.stringify({
+  //       date4: today,
+  //       status: { label: 'Add to backlog' },
+  //       email: { email: feedbackData.email, text: feedbackData.email },
+  //       text8: feedbackData.subject,
+  //       long_text: feedbackData.details,
+  //       text6: feedbackData.id,
+  //     }),
+  //   }
+  //   const mondayMutation5 = `mutation ($myItemName: String!, $column_values: JSON!){ create_item(board_id: 3748755036, group_id: "topics", item_name: $myItemName, 
+  //       column_values: $column_values
+  //       ) {  id  }  }`
+
+  //   return this.httpService
+  //     .post(
+  //       'https://api.monday.com/v2',
+  //       {
+  //         query: mondayMutation5,
+  //         variables: JSON.stringify(vars),
+  //       },
+  //       {
+  //         headers: headers,
+  //       },
+  //     )
+  //     .pipe(
+  //       catchError((error: any) => {
+  //         Logger.error('Error in sendFeedbackToMonday:', error.response.data)
+  //         throw 'An error happened!'
+  //       }),
+  //       tap((response) => Logger.log('info', response.data)),
+  //       map((response) => response.data),
+  //     )
+  // }
 }
